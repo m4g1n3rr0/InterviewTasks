14785831ca7710c4ba1878165524bb646c3a2081
petrlarionov <petrlarionov@localhost>
2023-11-17 11:07:00 +0200

    Two classes "MyStack" and "MyStackTest" are added with written tests

---
+++ IntervewTasks/src/telran/interview/MyStack.java
@@ -0,0 +1,63 @@
+package telran.interview;
+
+/**
+ * All the method implementations should be in the complexity O[1]
+ */
+
+public class MyStack {
+		
+	//TODO data structure fields
+	/**
+	* adds a given number at top of the stack
+	* @param number
+	*/
+	
+	
+		
+	public void push(int number) {
+	    	//TODO
+	
+	}
+	
+	/**
+	 * 
+	 * @return number and remove the number form the stack top
+	 * In the case the stack is empty the Exception (NoSuchElementException)
+	 * must be thrown
+	 */
+
+	public int pull() {
+			//TODO
+	
+		return -1;
+	
+	}
+	
+	/**
+	 * 
+	 * @return true if the stack is empty otherwise false
+	 */
+	
+	public boolean isEmpty() {
+			//TODO
+		
+		return false;
+		
+	}
+		
+	/**
+	 * 
+	 * @return maximal number existing in the stack
+	 * In the case the stack is empty the Exception (NoSuchElementException)
+	 * must be thrown
+	 */
+
+	public int getMax() {
+			//TODO
+			
+		return -1;
+		
+	}
+	
+}
+
